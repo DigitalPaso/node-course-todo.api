23964696f1a267945c0947f1353f425a68eae49c
Assana Fard <assana@scarlatti.local>
2018-11-05 13:55:13 -0800

    fixed database name

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,12 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true });
 
 
 //MONGODB_URI: mongodb://assana:pix2pash@ds135624.mlab.com:35624/nodeserverclass
 
-var url = process.env.MONGODB_URI || "mongodb://localhost:27017/yelp_camp";
-mongoose.connect(url);
+var url = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
+mongoose.connect(url, { useNewUrlParser: true });
 
 module.exports = {mongoose};
