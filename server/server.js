c7b98db487556d37855e88708003ad6f525ca9cb
Assana Fard <assana@scarlatti.local>
2018-11-05 12:57:58 -0800

    More routes and their tests

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-var express = require ('express');
-var bodyParser = require ('body-parser');
+var express 	= require ('express');
+var bodyParser 	= require ('body-parser');
+var {ObjectID} 	= require ('mongodb');
 
-var {mongoose} = require("./db/mongoose.js");
-var {Todo} = require ('./models/todo.js');
-var {User} = require ('./models/user.js');
+var {mongoose} 	= require("./db/mongoose.js");
+var {Todo} 	= require ('./models/todo.js');
+var {User} 	= require ('./models/user.js');
+
 
 var app = express();
 app.use(bodyParser.json());
@@ -31,6 +33,21 @@ app.get ('/todos', (req, res) => {
     );
 });
 
+app.get ('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id))  {
+	console.log ("Id not valid");
+	return res.status(404).send();
+    }
+    Todo.findById(id).then ((todo) => {
+	if (!todo) {
+	    return res.status(404).send();
+	}
+	res.status(200).send ({todo});
+    }).catch ((e) => {
+	res.status(400).send();
+    });
+});
 
 app.listen(3000, () => {
     console.log ("Server started on port 3000");
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require ('mongodb');
+
 
 const {app} = require ('../server.js');
 const {Todo} = require ('../models/todo.js');
 
 var todos = [
-    {text: "First test todo"},
-    {text: "Second test todo"}
+    {_id: new ObjectID(),
+    text: "First test todo"},
+    {_id: new ObjectID(),
+    text: "Second test todo"}
 ];
 
 beforeEach((done) => {
@@ -67,5 +71,36 @@ describe ('POST /todos', () =>
 	})
 	.end(done);
     });
+});
+
+describe ('GET /todos:id', () =>
+{
+    it ('should get a todo byId', (done) => {
+	request(app)
+	.get(`/todos/${todos[0]._id.toHexString()}`)
+	.expect(200)
+	.expect((res) => {
+	    expect(res.body.todo.text).toBe(todos[0].text);
+	})
+	.end(done);
+    });
+
+    if ('should return 404 if todo not found', (done) => {
+	var id = new ObjectID().toHexString();
+	request(app)
+	.get(`/todos/${id}`)
+	.expect(404)
+	.end(done);
+    });
+
+    if ('should return 404 if for invalid object ids ', (done) => {
+	var badId = '123';
+	request(app)
+	.get(`/todos/${badId.toHexString()}`)
+	.expect(404)
+	.end(done);
+    });
+
+
 });
 
