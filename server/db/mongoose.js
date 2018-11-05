c5cf1b9529beed91a7ffc483fed4a96fd08a7577
Assana Fard <assana@scarlatti.local>
2018-11-05 14:02:41 -0800

    Set DB URL

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,14 +1,12 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
+/*
 mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true });
 
-
-//MONGODB_URI: mongodb://assana:pix2pash@ds135624.mlab.com:35624/nodeserverclass
-
-/*
+MONGODB_URI: mongodb://assana:pix2pash@ds135624.mlab.com:35624/nodeserverclass
+*/
 var url = process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
 mongoose.connect(url, { useNewUrlParser: true });
-*/
 
 module.exports = {mongoose};
