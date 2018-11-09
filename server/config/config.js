a55323b1cf0542a03f6096dfc0fde7da028eda1a
Assana Fard <assana@scarlatti.local>
2018-11-09 15:02:18 -0800

    Log cleanup

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,12 @@
 // choices: production (set in heroku), test (set in packages.json, and development (set here)
 
 var env = process.env.NODE_ENV || 'development';
-console.log ("env:", env);
 
 if (env === 'development' || env === 'test') {
     var config = require ('./config.json');
     var envConfig = config[env];
-    console.log (envConfig);
 
     var keys = Object.keys(envConfig);
-    if (keys)
-	console.log (keys);
     keys.forEach((key) => {
 	process.env[key] = envConfig[key];
     });
