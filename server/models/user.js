8c5d3825a3560f650a8abd57b280c1e649ff2d00
Assana Fard <assana@scarlatti.local>
2018-11-09 09:24:35 -0800

    Delete token

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,13 @@ UserSchema.methods.generateAuthToken = function () {
     });
 }
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    return user.update({$pull:{
+	tokens: {token}
+    }});
+};
+
 UserSchema.statics.findByToken = function (token) {
     var User = this;	// Upper case... for model methods
     var decoded;
